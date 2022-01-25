7f76a96fa0d0d707aefc3fb02125685ebe56f5a9
Jonas <jonasklare4@gmail.com>
2022-01-25 12:25:04 -0600

    Added some outlines for new questions

--- src/Questions.tsx
+++ src/Questions.tsx
@@ -1,4 +1,41 @@
 export const Questions: string =
-`I'm stronger than all of my friends,str,1
+`I can do a lot of push ups,str,1
+I can climb ropes easily,str,1
+I'm known amongst my friends for performing feats of strength,str,1
+I'm stronger than all of my friends,str,1
 I'm cuter than all my friends,cha,1
-I'm sicker than all my friends,cha,1`
+I'm sicker than all my friends,cha,1,
+NEw QUESTIONS??s,dex,1`
+
+
+/**
+ * STR: Looking at strength levels
+ *      Strength comparison to friends
+ *      Take a look at skills u may have?
+ * DEX: Good at hand eye coordination, agility, reflexes, fine motor skills, speed
+ *      Skills
+ * CON: Acts of Stamina
+ *      Preventing sickness / strong immunity
+ *      Skills
+ * WIS: Keep a journal
+ *      Empathy, compassion
+ *      Emotional regulation?
+ *      Decisiveness
+ *      Social Advising
+ * INT: Highest level of education (manage state)
+ *      Seem to get things done quicker than friends
+ *      You ask a lot of questions
+ *      High school was easy
+ *      College was easy
+ * CHA: people see you as a natural leader
+ *      you feel confident in social situations
+ *      you're a persuasive person
+ *      people often comment on your personality
+ *      you recieve compliments on your appearance
+ *      
+ * 
+ * 
+ * Solitude (int) vs Groups (cha)
+ * Follow heart vs brain
+ * easier to continue walking or continue working
+ */
